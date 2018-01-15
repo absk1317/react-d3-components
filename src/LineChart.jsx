722a7dbbcb4dd8b29d4b889560df228486276a74
Abhishek Verma <abhishek.verma@cybrilla.com>
2018-01-15 23:52:14 +0530

    Use proper colors

--- src/LineChart.jsx
+++ src/LineChart.jsx
@@ -296,7 +296,7 @@ const LineChart = createReactClass({
                         className={lineStructureClassName}
                         d={d3.svg.symbol().type(customPointShape)()}
                         transform={translatePoints(p)}
-                        fill={colorScale(label(p))}
+                        fill={colorScale(label(d))}
                         onMouseEnter={evt => this.onMouseEnter(evt, data)}
                         onMouseLeave={evt => this.onMouseLeave(evt)}
                     />
